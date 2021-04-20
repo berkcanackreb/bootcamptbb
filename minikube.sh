8930c3e9b203174b9f6df2e4fc2c6f29a6675c24
oguz.zarci <oguz.zarci@bilgeadam.com>
2021-04-20 22:13:58 +0300

    minikube sh fix

--- minikube.sh
+++ minikube.sh
@@ -3,5 +3,5 @@ username=$(whoami)
 base64 C://Users//$username//.minikube/ca.crt > cacrt.txt
 
 base64 C://Users//$username//.minikube//profiles//minikube//client.crt > client.txt
-base64 C://Users//oguz.zarci//.minikube//profiles//minikube//client.key > clientkey.txt
+base64 C://Users//$username//.minikube//profiles//minikube//client.key > clientkey.txt
 cp C://Users//$username//.kube/config .
