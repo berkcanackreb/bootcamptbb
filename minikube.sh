a55cac4dcf3a1664b71c2cfd873be932d760ce98
Serkan Taşcı <83806460+serkantasci@users.noreply.github.com>
2021-06-17 14:02:13 +0300

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 username=$(whoami)
-base64 C://Users//$username//.minikube/ca.crt > cacrt.txt
-base64 C://Users//$username//.minikube//profiles//minikube//client.crt > client.txt
-base64 C://Users//$username//.minikube//profiles//minikube//client.key > clientkey.txt
-cat cacrt.txt | tr -d '\n' > cacrt.txt
-cat client.txt | tr -d '\n' > client.txt
-cat clientkey.txt | tr -d '\n' > clientkey.txt
-
+base64 C://Users//$username//.minikube/ca.crt > cacrt.txt | tr -d '\n' > cacrt.txt
+base64 C://Users//$username//.minikube//profiles//minikube//client.crt > client.txt  | tr -d '\n' > client.txt
+base64 C://Users//$username//.minikube//profiles//minikube//client.key > clientkey.txt  | tr -d '\n' > clientkey.txt
 cp C://Users//$username//.kube/config .
